4b94afa6b57b053dc8ef44ec2d55e83bd21a7ae9
souranshu76 <55708776+souranshu76@users.noreply.github.com>
2019-10-30 12:55:43 +0530

    Updated test

---
+++ Django_test/PRN/ui/test/view_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from ui import views
+
+# Create your tests here.
+
+class dashboardTest(TestCase):
+
+    def setUp(self):
+        pass
+    
+    
+    def test_dashboard(self):
+        pass
+    
+    def test_notes(self):
+        pass
+      
